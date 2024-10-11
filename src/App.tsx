e585b6edc2b4b9ec49019a1e2df5f972b8e1fb45
christokeerthi <chirstok@gmail.com>
2024-10-11 21:01:28 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import MuiNavbar from "./components/MuiNavbar";
+import MuiHomescreen from "./components/MuiHomescreen";
+import MuiApplication from "./components/MuiApplication"; // Ensure this path is correct
+
+import Box from "@mui/material/Box";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <MuiNavbar />
+
+      <MuiHomescreen />
+      <MuiApplication />
+    </Box>
   );
 }
 
